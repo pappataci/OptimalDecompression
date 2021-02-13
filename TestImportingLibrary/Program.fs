d3cc385cec292dd2426213265f7a03c5d1c4bb97
lucadm <77809678+gldimuro@users.noreply.github.com>
2021-02-12 20:03:07 -0500

    added code for testing ML.NET. Starting stress testing Lars' code

--- TestImportingLibrary/Program.fs
+++ TestImportingLibrary/Program.fs
@@ -1,12 +1,10 @@
 ﻿// Learn more about F# at https://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
-open Mammolo
-open System
 
 [<EntryPoint>]
 let main argv =
-    Console.WriteLine(Calculate.outputExample)
-    Console.Read() |> ignore
+     
+     
     printfn "%A" argv
     0 // return an integer exit code
