be62f290eb27091126af22796f5ed128207cfa72
lucadm <77809678+gldimuro@users.noreply.github.com>
2021-09-10 17:12:26 -0400

    ToPython transition functional and tested from the Python side

---
+++ TrinomialModel/TrinomialModelToPython.fs
@@ -0,0 +1,23 @@
+﻿namespace TrinomialModToPython
+
+module ToPython =
+    let internalActionStepTransition (initNode: Node, nextTime: double , nextDepth : double) =
+        let nextDepthTime = {Time = nextTime; Depth = nextDepth}
+        oneActionStepTransition initNode nextDepthTime
+    
+    let createInitNodeWithThesePressAtDepth( press0, press1, press2 , depth) = 
+        let envInfo = {Time = 0.0; Depth = depth}
+
+        let tensions = [|press0; press1;press2|]
+                        |> Array.map Tension
+        let externalPressures = depth2EnvPressures depth
+        let (zeroVec:double[])  = Array.zeroCreate  ( ModelDefinition.modelParams.Rates |> Array.length )  
+        
+        {EnvInfo = envInfo  
+         TissueTensions = tensions
+         ExternalPressures = externalPressures
+         InstantaneousRisk = zeroVec
+         IntegratedRisk = zeroVec
+         IntegratedWeightedRisk = zeroVec 
+         AccruedWeightedRisk = zeroVec 
+         TotalRisk  = 0.0}
