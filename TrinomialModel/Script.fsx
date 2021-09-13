a5673768289efc2a27754dfd027733661b23f6fe
lucadm <77809678+gldimuro@users.noreply.github.com>
2021-09-13 16:15:56 -0400

    strategy generator ultimated but to be tested

--- TrinomialModel/Script.fsx
+++ TrinomialModel/Script.fsx
@@ -36,15 +36,23 @@ type TrajectoryGenerator = | TrajGen of ( double -> Node  ->  DenseVector<float>
 
 let linPowerCurveGenerator  (decisionTime:double) (initialNode:Node) (curveParams:DenseVector<float>) : seq<DepthTime> = 
     
+    let breakFraction = curveParams.[0]
+    let powerCoeff = curveParams.[1] // this has to be positive
+    let tau = curveParams.[2] // this has to be positive
+ 
     let initialDepth = initialNode.EnvInfo.Depth
-    let targetDepth  = curveParams.[0] * initialDepth
+    let endLinearPartDepth  = breakFraction  * initialDepth
+    let endLinearPartTime   = (endLinearPartDepth - initialDepth)/ascentRate
+
+    let endLinearPartDepthTime = {Time = endLinearPartTime; Depth = endLinearPartDepth} 
 
     let getNextLinDepthTimeWTargetDepth (currentDepthAndTime:DepthTime) targetDepth = 
         (targetDepth - initialDepth)/ascentRate
 
-    let getNextLineDepthTime (currentDepthTime:DepthTime) deltaT = 
-        
+
+    let getNextLinDepthTime deltaT (currentDepthTime:DepthTime)  =      
         let tentativeTargetDepth = currentDepthTime.Depth + ascentRate * deltaT
+
         match tentativeTargetDepth with 
         | x when x < 0.0 -> { Depth  = 0.0
                               Time = -currentDepthTime.Depth / ascentRate } 
@@ -52,14 +60,38 @@ let linPowerCurveGenerator  (decisionTime:double) (initialNode:Node) (curveParam
         | _ -> {Depth = tentativeTargetDepth 
                 Time = currentDepthTime.Time + deltaT}
     
+    let getNextCurveDepthTime  (currentDepthTime: DepthTime)  deltaT=
+        if currentDepthTime.Depth < 0.0 then 
+            let evaluateCurveAtThisTime (evaluationTime:double) = 
+                endLinearPartDepth - endLinearPartDepth *  ( ( (evaluationTime - endLinearPartTime) / tau ) ** powerCoeff )
+        
+            let currentCurveValue = evaluateCurveAtThisTime (currentDepthTime.Time)
+            let nextCurveValueAtT = evaluateCurveAtThisTime (currentDepthTime.Time + deltaT)
 
-    //let getNextNonlinDepthTimeWTargetDepth 
+            let curveIncrement = max( nextCurveValueAtT - currentCurveValue) ascentRate*deltaT  
 
-         
+            let nextCandidateDepth = curveIncrement + currentDepthTime.Depth
 
+            let candidateDepthTime = {Time = currentDepthTime.Time + deltaT
+                                      Depth = curveIncrement + currentDepthTime.Depth}
+
+            match nextCandidateDepth with
+            | x when x < 0.0 -> { Depth = 0.0 
+                                  Time = -currentDepthTime.Depth / curveIncrement  }
+            | _ -> {Depth = candidateDepthTime.Depth
+                    Time = currentDepthTime.Time + deltaT}
+        else
+            {Depth = -999.9
+             Time  = currentDepthTime.Time }
+        
+    let nonlinearDecisionSeq = (fun _ -> decisionTime)
+                               |> Seq.initInfinite 
+                               |> Seq.scan getNextCurveDepthTime endLinearPartDepthTime
+                               |> Seq.takeWhile ( fun x -> x.Depth >= 0.0 )
     
-    seq{{Time = 0.0; Depth = 0.0}} // dummy result 
-
+    seq{yield endLinearPartDepthTime 
+        yield! nonlinearDecisionSeq }
+    
 
 
 //let linear
